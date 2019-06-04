b337ee709dbef3b3f63dbf9773e326fad8086ad6
rasakereh <rasakereh@manmail.ir>
2019-06-05 03:56:24 +0430

    sending url

---
+++ appVars.inc.php
@@ -0,0 +1,3 @@
+<?php
+$botToken = "821288398:AAFj7vf3L3hw0kLBGwsnkU__fzy8TmFxuMA"
+$gameURL = "https://rayanesh-game.herokuapp.com/text.htm"
--- webhook.php
+++ webhook.php
@@ -1,4 +1,48 @@
 <?php
-$fd = fopen("inputs.txt", "a");
-fprintf($fd, "%s\n", file_get_contents('php://input'));
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . 'appVars.inc.php';
+
+function storeInClass($className, $srcArray)
+{
+    try
+    {
+        $result = new $className();
+        foreach($srcArray as $key=>$value)
+        {
+            $result->$key = $value;
+        }
+
+        return $result;
+    }
+}
+
+function logInput()
+{
+    $fd = fopen("inputs.txt", "a");
+    fprintf($fd, "%s\n", file_get_contents('php://input'));
+}
+
+function main()
+{
+    logInput();
+    use Formapro\TelegramBot\AnswerCallbackQuery;
+    use Formapro\TelegramBot\Bot;
+    use Formapro\TelegramBot\Update;
+    
+    $bot = new Bot($botToken);
+
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    $update = Update::create($data);
+
+    if ($callbackQuery = $update->getCallbackQuery())
+    {
+        $answer = new AnswerCallbackQuery($callbackQuery->getId());
+        $answer->setUrl($gameURL);
+        $bot->answerCallbackQuery($answer);
+    }
+}
+
+main();
+
 ?>
