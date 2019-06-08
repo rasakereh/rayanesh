e7ce9bac18357631a46606c229c4a56bb8036e35
rasakereh <rasakereh@manmail.ir>
2019-06-08 12:43:09 +0430

    Updating Story

---
+++ updateStory.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/appVars.inc.php";
+
+function main()
+{
+    $story = "شهید علی‌اکبر غیوری ثالث در اولین بهار زندگی خود به دنیا آمد، اما گویا دنیا به وی نیامد
+    او پنج‌سال بعد وارد دانشکده‌ی هنرهای زیبا شد و از همان اول از همسن و سال‌های خود حداقل سه سال بزرگ‌تر بود
+    علی اکبر قصه‌ی ما، با صدور فرمان جهاد توسط امام(عج) به دمشق رفت و از آنجا یکی یکی پله‌های ترقی را طی نموده و پزشکی قهار شد.
+    او کسی نبود جز پروفسور سمیعی.
+    خون آریایی تو رگات نیست اگه به اشتراک نذاری";
+
+    $database = initDatabase();
+    $database->delete("story", ["1"=>"1"]);
+    $words = explode(" ",$story);
+    for($i = 0; $i < count($words); $i++)
+    {
+        $database->insert("story", ["word_place"=>$i, "word"=>$words[$i]]);
+    }
+    echo("done!");
+}
+
+main();
